a865b63919e7c9d59839f97e405e263a6c5ec897
paivo <paveee777@gmail.com>
2020-02-07 13:36:54 +0200

    lisättiin backend

---
+++ build/precache-manifest.e4082e5dbff1753f7d4265114c39f700.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e27b5156980d11487431ae6b8ddb7b69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c98c0264588cbbdeed39",
+    "url": "/static/css/main.e0b3f479.chunk.css"
+  },
+  {
+    "revision": "ddf188f431a25e544b31",
+    "url": "/static/js/2.1e399ce8.chunk.js"
+  },
+  {
+    "revision": "497d7e07705261348096eb742962e0f0",
+    "url": "/static/js/2.1e399ce8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c98c0264588cbbdeed39",
+    "url": "/static/js/main.318d75c3.chunk.js"
+  },
+  {
+    "revision": "881da45366845b30c975",
+    "url": "/static/js/runtime-main.f5e85168.js"
+  }
+]);
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var morgan = require('morgan')
 
 const app = express()
 app.use(express.json())
+app.use(express.static('build'))
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 const cors = require('cors')
 
